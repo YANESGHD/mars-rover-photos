864ec4fae05b6199c8730ca40dc2efcb1d2b8aa7
Gabriela Yanes <gabriela.yanes@geopagos.com>
2023-06-29 19:53:22 -0300

    fix: dependency conflicts

--- src/components/DetailsModal/DetailsModal.tsx
+++ src/components/DetailsModal/DetailsModal.tsx
@@ -20,11 +20,11 @@ export const DetailsModal: FC<DetailsModalProps> = ({ open, setOpen, details }) 
   return (
     <Dialog open={open} onClose={() => setOpen(false)}>
       <Container>
-        <Image src={details.img_src} alt="Image" />
+        <Image src={details.img_src} />
         <TextContainer>
-          <Typography variant="body1">{`Earth Date: ${details.earth_date}`}</Typography>
-          <Typography variant="body1">{`Sol Date: ${details.sol}`}</Typography>
-          <Typography variant="body1">{`Camera: ${details.camera.full_name}`}</Typography>
+          <Typography variant='body1'>{`Earth Date: ${details.earth_date}`}</Typography>
+          <Typography variant='body1'>{`Sol Date: ${details.sol}`}</Typography>
+          <Typography variant='body1'>{`Camera: ${details.camera.full_name}`}</Typography>
         </TextContainer>
         <BookmarkButton onClick={handleBookmarkedImage}>
           {validateBookmark(details) ? <Bookmark sx={{ color: '#FFD000', fontSize: 35 }} /> : <BookmarkBorder sx={{ fontSize: 35 }} />}
